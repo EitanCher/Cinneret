af72bb01c444dcbf84bebd36281fb1c72c99abd7
EitanCher <102179966+EitanCher@users.noreply.github.com>
2022-08-21 21:18:45 +0300

    Update Class_20220822.cs

--- C_Sharp/Year1_Summer/ClassWork/Class_20220822.cs
+++ C_Sharp/Year1_Summer/ClassWork/Class_20220822.cs
@@ -13,6 +13,51 @@ namespace ConsoleApp3
             // ג. לכתוב תכנית המגדירה מערך ובעזרת הפועלה מסעיף ב מדפיסה 
             // את כל איברי המערך כאשר ליד המספרים בעלי סכום הספרות המקסימלי יש גם *
             ===========================================================*/
+            
+            ex(1);
+        }
+        
+        
+        static void ex1()
+        {
+            int[] myArr = { 1, 2, 445, 9 };
+            int maxVal = getMax(myArr);
+
+            // C:
+            for (int i = 0; i < myArr.Length; i++)
+            {
+                if (getSum(myArr[i]) == getMax(myArr))
+                {
+                    Console.WriteLine($"{myArr[i]} *");
+                }
+                else
+                {
+                    Console.WriteLine($"{myArr[i]} ");
+                }
+            }
+        }
+
+        // B: Get an array and find its max sum of digits, using #A 
+        public static int getMax(int[] myInputArray)
+        {
+            int myMax = getSum(myInputArray[0]);
+            for (int i = 1; i < myInputArray.Length; i++)
+                if (getSum(myInputArray[i]) > myMax)
+                    myMax = getSum(myInputArray[i]);
+            
+            return myMax;
+        }
+
+        // A: Get a number and calculate sum of its digits:
+        public static int getSum(int myInput)
+        {
+            int myOutput = 0;
+            while (myInput != 0)
+            {
+                myOutput += myInput % 10;
+                myInput /= 10;
+            }
+            return myOutput;
         }
     }
 }
